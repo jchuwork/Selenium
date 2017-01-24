15e5556daeff16eaea205c59a9bdc067886b84dc
jchuwork <jonathan@challengepost.com>
2017-01-24 12:20:51 -0500

    Update ChromeCreateSoftwareProject.java

--- ChromeCreateSoftwareProject.java
+++ ChromeCreateSoftwareProject.java
@@ -20,10 +20,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 public class ChromeCreateSoftwareProject {
 
-	
 	private WebDriver driver;
 	
 	@Before
@@ -31,11 +29,10 @@ public class ChromeCreateSoftwareProject {
 		
 	        System.setProperty("webdriver.chrome.driver", "/Users/jchuwork/Documents/workspace/Webdriver/chromedriver");
 		    
-		    ChromeOptions options = new ChromeOptions(); 
-		    options.addArguments("user-data-dir=/Users/portia/Library/Application Support/Google/Chrome/SeleniumProfile");
-		    driver = new ChromeDriver(options);
-		 
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+   		ChromeOptions options = new ChromeOptions(); 
+		options.addArguments("user-data-dir=/Users/jchuwork/Library/Application Support/Google/Chrome/SeleniumProfile");
+		driver = new ChromeDriver(options);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 	
 
@@ -48,80 +45,66 @@ public class ChromeCreateSoftwareProject {
 	@Test
 	public void test() {
 	
-		    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
 		
+		driver.get("http://staging.devpost.com/opensesame/github");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElement(By.linkText("Log in")).click();
+		driver.findElement(By.id("user_email")).sendKeys("jonathan.chu.cp@gmail.com");
+	        driver.findElement(By.id("user_password")).sendKeys("challenge2015");
+		driver.findElement(By.id("submit-form")).click();
+		System.out.println("Logging In Successfully on FF"); 
 		
-			driver.get("http://staging.devpost.com/opensesame/github");
-			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-			driver.findElement(By.linkText("Log in")).click();
+		driver.get("https://staging.devpost.com/software");
+		driver.findElement(By.id("add-your-software")).click();
+		driver.findElement(By.id("software_name")).sendKeys(timeStamp + "  Automated Selenium Project - FF");
+		driver.findElement(By.id("software_name_save_button")).click();
+		driver.getTitle();
+		System.out.println(driver.getTitle());
+		assertEquals("Devpost", driver.getTitle());  //assertion page title test
 			
-			
-			
-			driver.findElement(By.id("user_email")).sendKeys("jonathan.chu.cp@gmail.com");
-			driver.findElement(By.id("user_password")).sendKeys("challenge2015");
-			driver.findElement(By.id("submit-form")).click();
-			System.out.println("Logging In Successfully on FF"); 
+		System.out.println("Adding a project to your portfolio.");  
 		
-			driver.get("https://staging.devpost.com/software");
-			driver.findElement(By.id("add-your-software")).click();
-			driver.findElement(By.id("software_name")).sendKeys(timeStamp + "  Automated Selenium Project - FF");
-			driver.findElement(By.id("software_name_save_button")).click();
-			driver.getTitle();
-			System.out.println(driver.getTitle());
-			assertEquals("Devpost", driver.getTitle());  //assertion page title test
-			
-			System.out.println("Adding a project to your portfolio.");  
-			
-
-			
-			driver.findElement(By.id("software_tagline")).sendKeys("Tagline");
-			System.out.println("Adding a tagline.");
+		driver.findElement(By.id("software_tagline")).sendKeys("Tagline");
+		System.out.println("Adding a tagline.");
 	
-			driver.findElement(By.id("email")).sendKeys("jonathan.chu.cp+3@gmail.com");
-			driver.findElement(By.className("attach-on-right")).click(); 
+		driver.findElement(By.id("email")).sendKeys("jonathan.chu.cp+3@gmail.com");
+		driver.findElement(By.className("attach-on-right")).click(); 
+		driver.findElement(By.id("software_description")).sendKeys("Description");
+		System.out.println("Adding a description.");  
 			
-								
+		driver.findElement(By.id("s2id_autogen1")).sendKeys("selenium");
+		waitforelements(1000);  //Need this pause so the enter key works to select the skill
+		driver.findElement(By.id("s2id_autogen1")).sendKeys(Keys.RETURN);
+		System.out.println("Adding a skill.");  
 			
-			driver.findElement(By.id("software_description")).sendKeys("Description");
-			System.out.println("Adding a description.");  
+		driver.findElement(By.id("software_urls_attributes_0_url")).sendKeys("http://staging.devpost.com");
+		System.out.println("Adding software URL");  
 			
-			driver.findElement(By.id("s2id_autogen1")).sendKeys("selenium");
-			waitforelements(1000);  //Need this pause so the enter key works to select the skill
-			driver.findElement(By.id("s2id_autogen1")).sendKeys(Keys.RETURN);
-			System.out.println("Adding a skill.");  
+		driver.findElement(By.id("software_video_url")).sendKeys("https://www.youtube.com/watch?v=YroljNRBy6Q"); 
+		System.out.println("Adding video URL");  
 			
-			driver.findElement(By.id("software_urls_attributes_0_url")).sendKeys("http://staging.devpost.com");
-			System.out.println("Adding software URL");  
-			
-			driver.findElement(By.id("software_video_url")).sendKeys("https://www.youtube.com/watch?v=YroljNRBy6Q"); 
-			System.out.println("Adding video URL");  
-			
-						
-			JavascriptExecutor click = (JavascriptExecutor)driver;  //Javascript is needed to upload the image.  
-			WebElement fileInput = driver.findElement(By.id("software_photo_data")); 
-			String js = "arguments[0].style.display ='block';";
-			// String js = "$('#software_photo_data').css(display: 'block');";   jquery version
+		JavascriptExecutor click = (JavascriptExecutor)driver;  //Javascript is needed to upload the image.  
+		WebElement fileInput = driver.findElement(By.id("software_photo_data")); 
+		String js = "arguments[0].style.display ='block';";
+		// String js = "$('#software_photo_data').css(display: 'block');";   jquery version
     		click.executeScript(js, fileInput);
     		driver.findElement(By.id("software_photo_data")).sendKeys("/Users/portia/Documents/workspace/Webdriver/seleniumimage.jpg");
     		System.out.println("Image uploaded successfully");
 			
 			
-			driver.findElement(By.id("software-save")).click();
-			
-			driver.findElement(By.className("option-no")).click();
-			
-			WebElement element = driver.findElement(By.id("global-nav-user-dropdown"));
-			Actions action = new Actions(driver);
-		    action.moveToElement(element).build().perform();
-		    waitforelements(2000);
-		    driver.findElement(By.linkText("Log out")).click();
-			System.out.println("Logged Out Successfully on Chrome"); 
+		driver.findElement(By.id("software-save")).click();
+		driver.findElement(By.className("option-no")).click();
+		WebElement element = driver.findElement(By.id("global-nav-user-dropdown"));
+		Actions action = new Actions(driver);
+		action.moveToElement(element).build().perform();
+		waitforelements(2000);
+		driver.findElement(By.linkText("Log out")).click();
+		System.out.println("Logged Out Successfully on Chrome"); 
 		    
-			waitforelements(3000);     
+		waitforelements(3000);     
 															
-			System.out.println("Successfully created a software project!"); 
-			
-			
+		System.out.println("Successfully created a software project!"); 
 			
 		}		
 	
@@ -134,8 +117,7 @@ public class ChromeCreateSoftwareProject {
 		catch(InterruptedException ie){
 			
 		}
-		
-		
+				
 	}
 	
 }
